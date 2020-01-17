c239ba2c6d24a57d78d7509793a241931c0f1269
Diego <diegofernades@gmail.com>
2020-01-17 12:38:14 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormControl, FormsModule } from '@angular/forms';
+import { MenuComponent } from './components/menu/menu.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ListaComponent } from './components/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastroComponent,
+    ListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    // FormControl,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
